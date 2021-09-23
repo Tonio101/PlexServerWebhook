e18ef6581dd69d852123123bb6c2912333b20d90
Antonio <antonio@antonio101.com>
2021-09-22 20:25:25 -0700

    Plex webhook service

--- start_plex_webhook.sh
+++ start_plex_webhook.sh
@@ -4,7 +4,7 @@ FLOCK=/usr/bin/flock
 LOCK_FILE=/tmp/plexyhook.lockfile
 FLOCK_OPTS="-n"
 
-WEBHOOK_SCRIPT=$HOME/DEV/PlexServerWebhook/src/plexy_webhook.py
+WEBHOOK_SCRIPT=$HOME/PlexServerWebhook/src/plexy_webhook.py
 WEBHOOK_ARGS="--config src/plex_webhook_config.local.yml"
 
 $FLOCK $FLOCK_OPTS $LOCK_FILE $WEBHOOK_SCRIPT $WEBHOOK_ARGS
